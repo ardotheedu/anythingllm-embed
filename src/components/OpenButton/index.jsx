7504959c493d534009c407f4ff9c240016976f41
ardotheedu <eduardosalima3@gmail.com>
2025-09-29 09:24:32 -0300

    feat: adiciona imagem para o botão minimizado

--- src/components/OpenButton/index.jsx
+++ src/components/OpenButton/index.jsx
@@ -18,18 +18,29 @@ const CHAT_ICONS = {
 
 export default function OpenButton({ settings, isOpen, toggleOpen }) {
   if (isOpen) return null;
-  const ChatIcon = CHAT_ICONS.hasOwnProperty(settings?.chatIcon)
-    ? CHAT_ICONS[settings.chatIcon]
-    : CHAT_ICONS.plus;
   return (
     <button
-      style={{ backgroundColor: settings.buttonColor }}
+      style={{
+        backgroundColor: settings.buttonColor,
+        padding: 0,
+        width: 56,
+        height: 56,
+      }}
       id="anything-llm-embed-chat-button"
       onClick={toggleOpen}
-      className={`hover:allm-cursor-pointer allm-border-none allm-flex allm-items-center allm-justify-center allm-p-4 allm-rounded-full allm-text-white allm-text-2xl hover:allm-opacity-95`}
+      className={`hover:allm-cursor-pointer allm-border-none allm-flex allm-items-center allm-justify-center allm-rounded-full allm-text-white allm-text-2xl hover:allm-opacity-95`}
       aria-label="Toggle Menu"
     >
-      <ChatIcon className="text-white" />
+      <img
+        src={settings.brandImageUrl}
+        alt="Brand"
+        style={{
+          width: "100%",
+          height: "100%",
+          objectFit: "cover",
+          borderRadius: "50%",
+        }}
+      />
     </button>
   );
 }
